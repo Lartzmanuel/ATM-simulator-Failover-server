feb1429dd9e9874966265c8e029814d2acd28168
Lartzmanuel <teyeemmanuel01@gmail.com>
2024-05-16 00:18:04 +0000

    updated changes

--- public/js/script.js
+++ public/js/script.js
@@ -8,55 +8,118 @@
 //     } )
 // })
 
-document.querySelector('.card-number-input').oninput = () => {
-    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value
-}
 
-document.querySelector('.card-holder-input').oninput = () => {
-    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value
-}
+   
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"]
 
-document.querySelector('.month-input').oninput = () => {
-    document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value
-}
+    const date = new Date(),
+    day = date.getDate(),
+    year = date.getFullYear(),
+    month = monthNames[date.getMonth()]
 
-document.querySelector('.year-input').oninput = () => {
-    document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value
-}
+    //Add the appropriate suffix to the day
+    let dateWithSuffix = addSuffix(day);
 
-document.querySelector('.cvv-input').onmouseenter = () => {
-    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
-    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
-}
+    let dateFormat = `${dateWithSuffix} ${month.toUpperCase()}, ${year} |`
 
-document.querySelector('.cvv-input').onmouseleave = () => {
-    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
-    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
-}
+    const newDate = document.getElementById('date');
 
-document.querySelector('.cvv-input').oninput = () => {
-    document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
-}
+    newDate.innerHTML = dateFormat;
 
 
-const modal = document.getElementById('myModal');
-const btn = document.getElementById('logoutBtn');
-const span = document.getElementById('closeBtn');
-
-btn.onclick = () => {
-    modal.style.display = 'block';
-}
-
-function modal() {
-    modal.style.display = 'block';
-}
-
-span.onclick = () => {
-    modal.style.display = 'none';
-}
-
-window.onclick = () => {
-    if(e.target == modal) {
-        modal.style.display = 'none';
+    function addSuffix(day){
+        if(day > 3 && day < 21) return day + "TH";
+        switch(day % 10) {
+            case 1:  return day + "ST";
+            case 2:  return day + "ND";
+            case 3:  return day + "RD";
+            default: return day + "TH";
+        }
     }
-}
+
+
+    function displayTime() {
+        const time = document.getElementById('time');
+
+        let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes();
+    
+         //Set am or Pm
+         const amPm = hour >= 12? 'PM' : 'AM'
+    
+        //12 hr format
+        hour = hour % 12 || 12
+
+        time.innerHTML = `${addZero(hour)}:${addZero(min)} ${amPm}`  
+
+        setTimeout(displayTime, 1000)
+    }
+
+    function addZero(n) {
+        return(parseInt(n , 10) < 10? '0' : '') + n;
+    }
+    
+    displayTime()
+    addZero()
+ 
+    
+    document.querySelector('.card-number-input').oninput = () => {
+        document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value
+    }
+    
+    document.querySelector('.card-holder-input').oninput = () => {
+        document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value
+    }
+    
+    document.querySelector('.month-input').oninput = () => {
+        document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value
+    }
+    
+    document.querySelector('.year-input').oninput = () => {
+        document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value
+    }
+    
+    document.querySelector('.cvv-input').onmouseenter = () => {
+        document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+        document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+    }
+    
+    document.querySelector('.cvv-input').onmouseleave = () => {
+        document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
+        document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+    }
+    
+    document.querySelector('.cvv-input').oninput = () => {
+        document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
+    }
+    
+    
+
+    function displayModal(){
+        const myModal = document.getElementById('myModal');
+        const btn = document.getElementById('logoutBtn');
+
+        btn.onclick = () => {
+            myModal.style.display = 'block';
+        }
+        
+        window.onclick = (event) => {
+            if(event.target == myModal ) {
+                myModal.style.display = 'none';
+            }
+        }
+        
+    }
+
+    function Close() {
+        const myModal = document.getElementById('myModal');
+            myModal.style.display = 'none';
+    }
+
+    function Logout(){
+        window.location.href = '../../views/pin'
+    }
+
+    
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=> {
+router.get('/pin', (req, res)=> {
 
     const locals = {
         title: "Authentication",
